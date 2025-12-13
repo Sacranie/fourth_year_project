ea9d0f458f770c4cf8b5c707f4816825580fe36b
Husayn Sacranie <sacraniehusayn@gmail.com>
2025-12-13 13:21:02 +0000

    updated pricingLP to GlobalPricingLP to remain consistent

--- eac/__init__.py
+++ eac/__init__.py
@@ -1,13 +1,13 @@
-from .models import SellOrder, BuyOrder, Basket
+from .models import SellOrder, BuyOrder, Basket, MultiProductOrder
 from .Validators import build_loop_families, validate_unit_capacity
-from .PricingLP import PricingLP
+from .PricingLP import GlobalPricingLP
 from .Volume import VolumeMILP
 from .rounding import round_price_up_to_cent, rounding_and_residual_distribution
 from .orchestrator import run_market
 
 __all__ = [
-"SellOrder", "BuyOrder", "Basket",
+"SellOrder", "BuyOrder", "Basket", "MultiProductOrder",
 "build_loop_families", "validate_unit_capacity",
-"PricingLP", "VolumeMILP", "run_market",
+"GlobalPricingLP", "VolumeMILP", "run_market",
 "round_price_up_to_cent", "rounding_and_residual_distribution",
 ]
